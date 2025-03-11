dd266cfda8fa3b4775021a46ea6fef6efadb608b
Imdadul Ahmed <ahmedimdad1593@gmail.com>
2025-03-11 23:23:46 +0600

    add postinstall script

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -21,7 +21,7 @@ const DashboardRoute = async () => {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
-  const data = await getData(user.id);
+  const data = await getData(user?.id);
   return (
     <div>
       <div className="flex items-center justify-between mb-4">
